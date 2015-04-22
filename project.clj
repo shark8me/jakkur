01d3bf0a53ef6be33d055b7c9284602e4a83f4ab
kirankarkera <kiran.karkera@bridgei2i.com>
2015-04-22 14:52:38 +0530

    adding data folder

--- project.clj
+++ project.clj
@@ -4,5 +4,6 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :main sb.core
-  :dependencies [[org.clojure/clojure "1.5.1"]
+  :aot [sb.core]
+  :dependencies [[org.clojure/clojure "1.6.0"]
                 [org.clojars.strongh/mallet "2.0.7-SNAPSHOT"]])
